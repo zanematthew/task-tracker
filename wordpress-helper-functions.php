b7261673332ddd68ddba8815e0d01f2bc0e5a606
Zane M. Kolnik <zanematthew@gmail.com>
2011-07-22 19:25:07 -0700

    removed optgroup debugging code

--- wordpress-helper-functions.php
+++ wordpress-helper-functions.php
@@ -276,8 +276,7 @@ function zm_base_build_options( $taxonomy=null, $value='term_id' ) {
 	    <?php foreach( $terms as $term ) : ?>
             <?php /** Some cryptic short hand true:false */ ?>
             <?php $current_term == $term->name ? $selected = 'selected=selected' : $selected = null; ?>
-            <optgroup label="my label" />
-            <option value="<?php echo $term->$value; ?>" my_term_id=<?php echo $term->term_id; ?> <?php echo $selected; ?>><?php echo $term->name; ?></option>              
+            <option value="<?php echo $term->$value; ?>" my_term_id=<?php echo $term->term_id; ?> <?php echo $selected; ?>><?php echo $term->name; ?></option>
 	    <?php endforeach; ?>
     </select>
     </fieldset>
