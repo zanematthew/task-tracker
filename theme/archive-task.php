9274fa7c019642c4a23032e3d81d2601bc4b4268
Zane M. Kolnik <me@my-mac>
2011-08-08 23:36:23 -0400

    added missing dom ID, all taxes are no set to link = false.

--- theme/archive-task.php
+++ theme/archive-task.php
@@ -2,67 +2,71 @@
 <?php get_template_part( 'header-container', 'single' ); ?>
 <div class="container_12">
     <div class="grid_12">
-<div class="zm-tt-container zm-tt-archive-container">
-        <div class="main-container">
-            <div class="grid_9 alpha">
-                <?php load_template( MY_PLUGIN_DIR . 'theme/navigation.php' ); ?>   
-                <div id="tt_main_target">
-               <table>
-                    <thead>
-                        <tr>
-                            <th id="title"><span>Title</span></th>
-                            <th id="milestone"><span>Milestone</span></th>
-                            <th id="age"><span>Age</span></th>
-                            <th id="status"><span>Status</span></th>
-                            <th id="priority"><span>Priority</span></th>
-                            <th id="project"><span>Project</span></th>
-                        </tr>
-                    </thead>
-                    <?php $x = 0; ?>
-                    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-                        <tr <?php post_class('result')?>>
-                            <td><?php $x++; ?>
-                                 <strong class="title"><a href="<?php the_permalink(); ?>" title="Link to project: <?php the_title(); ?>">#<?php the_ID(); ?> <?php the_title(); ?></a></strong>
-                                 <span class="comment-count"><?php comments_number(' '); ?></span>
-                                 <div class="utility-container zm-base-hidden">
-                                     <?php edit_post_link('Admin Edit', '' , ' |'); ?>
-                                     by <?php the_author(); ?> on <?php the_time(get_option('date_format')); ?> |
-                                 </div>
-                            </td>
-                            <td>
-<div class="milestone-container zm-base-item">
-<?php print zm_base_get_the_term_list( array( 'link' => false, 'post_id' => $post->ID, 'taxonomy' => 'milestone' )); ?>
-</div>
-                            </td>
-                            <td>
-                            <?php project_age(); ?>
-                            </td>
-                            <td>
-      <div class="status-container"><?php print zm_base_get_the_term_list( $post->ID, 'status' ); ?></div>
-                            </td>
-                            <td>
-<div class="priority-container zm-base-item">
-<?php print zm_base_get_the_term_list( array( 'link' => false, 'post_id' => $post->ID, 'taxonomy' => 'priority' ) ); ?>
-</div>
-                            <td>
-<div class="project-container zm-base-item"><?php print zm_base_get_the_term_list( array( 'link' => false, 'post_id' => $post->ID, 'taxonomy' => 'project' ) ); ?></div>
-                            </td>
-                        </tr>
-                    <?php endwhile; ?>
-                </table>
-                    <?php // load_template( MY_PLUGIN_DIR . 'theme/archive-table.php' ); ?>
+        <div class="zm-tt-container zm-tt-archive-container">
+            <div class="main-container">
+                <div class="grid_9 alpha">
+                    <?php load_template( MY_PLUGIN_DIR . 'theme/navigation.php' ); ?>   
+                    <div id="tt_main_target">
+                       <table id="archive_table">
+                            <thead>
+                                <tr>
+                                    <th id="title"><span>Title</span></th>
+                                    <th id="milestone"><span>Phase</span></th>
+                                    <th id="age"><span>Age</span></th>
+                                    <th id="status"><span>Status</span></th>
+                                    <th id="priority"><span>Priority</span></th>
+                                    <th id="project"><span>Project</span></th>
+                                </tr>
+                            </thead>
+                            <?php $x = 0; ?>
+                            <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+                                <tr <?php post_class('result')?>>
+                                    <td><?php $x++; ?>
+                                         <strong class="title"><a href="<?php the_permalink(); ?>" title="Link to project: <?php the_title(); ?>">#<?php the_ID(); ?> <?php the_title(); ?></a></strong>
+                                         <span class="comment-count"><?php comments_number(' '); ?></span>
+                                         <div class="utility-container zm-base-hidden">
+                                             <?php edit_post_link('Admin Edit', '' , ' |'); ?>
+                                             by <?php the_author(); ?> on <?php the_time(get_option('date_format')); ?> |
+                                         </div>
+                                    </td>
+                                    <td>
+                                        <div class="milestone-container zm-base-item">
+                                            <?php print zm_base_get_the_term_list( array( 'link' => false, 'post_id' => $post->ID, 'taxonomy' => 'phase' )); ?>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <?php project_age(); ?>
+                                    </td>
+                                    <td>
+                                        <div class="status-container">
+                                            <?php print zm_base_get_the_term_list( array( 'link' => false, 'post_id' => $post->ID, 'taxonomy' => 'status' ) ); ?>                                            
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="priority-container zm-base-item">
+                                            <?php print zm_base_get_the_term_list( array( 'link' => false, 'post_id' => $post->ID, 'taxonomy' => 'priority' ) ); ?>
+                                        </div>
+                                    <td>
+                                        <div class="project-container zm-base-item">
+                                            <?php print zm_base_get_the_term_list( array( 'link' => false, 'post_id' => $post->ID, 'taxonomy' => 'project' ) ); ?>
+                                        </div>
+                                    </td>
+                                </tr>
+                            <?php endwhile; ?>
+                        </table>
+                        <?php // load_template( MY_PLUGIN_DIR . 'theme/archive-table.php' ); ?>
+                    </div>
+                </div>
+                <div class="grid_3 omega zm-tt-sidebar-container">
+                    <?php zm_base_list_terms( array('taxonomy' => 'status', 'link' => false ) ); ?>
+                    <?php zm_base_list_terms( array('taxonomy' => 'priority', 'link' => false ) ); ?>
+                    <?php zm_base_list_terms( array('taxonomy' => 'project', 'link' => false ) ); ?>
+                    <?php zm_base_list_terms( array('taxonomy' => 'phase', 'link' => false ) ); ?>
+                    <?php zm_base_list_terms( array('taxonomy' => 'assigned', 'link' => false ) ); ?>
                 </div>
             </div>
-            <div class="grid_3 omega zm-tt-sidebar-container">
-                <?php zm_base_list_terms( array('taxonomy' => 'status', 'link' => false ) ); ?>
-                <?php zm_base_list_terms( array('taxonomy' => 'priority', 'link' => false ) ); ?>
-                <?php zm_base_list_terms( array('taxonomy' => 'project', 'link' => false ) ); ?>
-                <?php zm_base_list_terms( array('taxonomy' => 'phase', 'link' => false ) ); ?>
-                <?php zm_base_list_terms( array('taxonomy' => 'assigned', 'link' => false ) ); ?>
-            </div>
-            </div>
+        </div>
     </div>
-</div>
 </div>
 <?php tt_json_feed(); ?>
 <?php get_footer(); ?>
