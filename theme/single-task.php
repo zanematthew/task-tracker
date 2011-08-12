e297ab279debc22393ba671cb1bd4550da9903b1
Zane M. Kolnik <me@my-mac>
2011-08-12 07:34:12 -0400

    removed next prev

--- theme/single-task.php
+++ theme/single-task.php
@@ -8,12 +8,6 @@
                 <?php load_template( MY_PLUGIN_DIR . '/theme/navigation.php' ); ?>   
                 <div id="tt_main_target">
                 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-                    <div class="nav-previous-container">
-                    <ul>
-                        <li class="previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></li>
-                        <li class="next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></li>
-                    </ul>
-                    </div>
                     <div <?php post_class('result')?>>
                         <h1 class="title"><?php the_title(); ?></h1>
                         <?php the_content(); ?>
