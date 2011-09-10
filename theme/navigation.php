a5d066720071639b66bd164fd508e5cbf953450d
Zane M. Kolnik <zanematthew@gmail.com>
2011-09-10 01:32:59 -0400

    typo

--- theme/navigation.php
+++ theme/navigation.php
@@ -1,7 +1,7 @@
 <div class="zm-tt-navigation-container">
     <nav class="tt-navigation-container">
         <ul>
-            <li class="currnet-item"><a href="<?php bloginfo('url'); ?>/task" title="List Recent Activity for: <?php bloginfo('name'); ?>">Recent</a></li>
+            <li class="current-item"><a href="<?php bloginfo('url'); ?>/task" title="List Recent Activity for: <?php bloginfo('name'); ?>">Recent</a></li>
             <li class="create-ticket">
                 <?php if ( is_user_logged_in() ) : ?>
                     <div class="zm-tt-form-container">
