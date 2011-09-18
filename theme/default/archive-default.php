e440e3da1bae64343583556500829b1ccb9f208c
Zane M. Kolnik <zanematthew@gmail.com>
2011-09-18 01:28:08 -0400

    trim whitespace

--- theme/default/archive-default.php
+++ theme/default/archive-default.php
@@ -3,29 +3,31 @@
 <?php 
 
 global $wp_query; 
-$my_cpt = $wp_query->query_vars['post_type'];
+$cpt = $wp_query->query_vars['post_type'];
+$cpt_obj = get_post_types( array( 'name' => $cpt), 'objects' );
 
 ?>
-    <div class="zm-tt-container zm-tt-archive-container">
-        <div class="tt-glue">
-            <div class="main-container">
-                <div id="tt_filter_target"></div>                
-                <div id="tt_main_target">
-                    <div class="tt_loading"></div>
-                    <div class="sample" tt_template="theme/default/archive-table.php" data-post_type="<?php print $my_cpt; ?>"></div> 
-                    <div id="no_results">nothing here move on..</div>                        
-                </div>
+<div class="zm-tt-container zm-tt-archive-container">
+    <div class="tt-glue">
+        <div class="main-container">        
+            <div class="tt-filter-container">
+                <ul class="inline">
+                    <li><a href="javascript://" id="filter_handle" tt_template="default/navigation-filter.php" data-post_type="<?php print  $cpt;?>">Advanced Filter</a></li>
+                </ul>
             </div>
-            <div class="zm-tt-sidebar-container">
-                <?php                        
-                $current_post_type = get_query_var( 'post_type' );                
-                $my_cpt = get_post_types( array( 'name' => $current_post_type), 'objects' );
-                ?>
-                <?php foreach ( $my_cpt[ $current_post_type ]->taxonomies as $tax ) : ?>
-                    <?php zm_base_list_terms( array('taxonomy' => $tax ) ); ?>
-                <?php endforeach; ?>        
+            <div id="tt_filter_target"></div>                
+            <div id="tt_main_target">
+                <div class="tt_loading"></div>
+                <div class="sample" tt_template="theme/default/archive-table.php" data-post_type="<?php print $cpt; ?>"></div> 
+                <div id="no_results">nothing here move on..</div>                        
             </div>
         </div>
+        <div class="zm-tt-sidebar-container">
+            <?php foreach ( $cpt_obj[ $cpt ]->taxonomies as $tax ) : ?>
+                <?php zm_base_list_terms( array('taxonomy' => $tax ) ); ?>
+            <?php endforeach; ?>        
+        </div>
     </div>
-<?php tt_json_feed( $current_post_type,  $my_cpt[ $current_post_type ]->taxonomies ); ?>
+</div>
+<?php tt_json_feed( $cpt,  $cpt_obj[ $cpt ]->taxonomies ); ?>
 <?php get_footer(); ?>
