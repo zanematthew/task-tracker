dce47d7d37950b7a0eeaf9e12d861a34c541420e
Zane M. Kolnik <zanematthew@gmail.com>
2011-10-24 11:22:44 -0400

    #84 added check if dialog is visible, so this only clears the visible form

--- theme/js/script.js
+++ theme/js/script.js
@@ -336,7 +336,10 @@ jQuery(document).ready(function( $ ){
             .val('')
             .removeAttr('checked')
             .removeAttr('selected');   
-        $('.ui-dialog:first').effect("highlight", {}, 3000);            
+        
+        if ( $('.ui-dialog').css('display') != 'none' ) {
+            $('.ui-dialog:first').effect("highlight", {}, 3000);
+        }
     }
 
     /** @todo clear [task] event: needs to be part of class for dialog */    
