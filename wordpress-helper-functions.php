217a6a57d69478558dc87702147c3e8acce45cc0
Zane M. Kolnik <zanematthew@gmail.com>
2011-10-25 20:43:23 -0400

    #97 fixed css class in wp helpers file

--- wordpress-helper-functions.php
+++ wordpress-helper-functions.php
@@ -393,11 +393,12 @@ function zm_base_build_input( $taxonomy=null ) {
         
     ?>    
     <?php if ( $terms ) : ?>    
-    <fieldset class="zm-base-<?php echo $taxonomy; ?>-container"><legend class="zm-base-title"><?php echo $label; ?></legend>
+    <fieldset class="<?php echo $taxonomy; ?>-container">
+    <legend class="zm-base-title"><?php echo $label; ?></legend>
     <?php foreach( $terms as $term ) : ?>
         <?php /** Some cryptic short hand true:false */ ?>
         <?php $current_term == $term->name ? $selected = 'checked=checked' : $selected = null; ?>
-        <label for="<?php echo $term->$value; ?>">        
+        <label for="<?php echo $term->$value; ?>" class="zm-base-<?php print $taxonomy; ?>-<?php print $term->slug; ?>">        
         <input type="<?php echo $type; ?>" value="<?php echo $prepend; ?><?php echo $term->$value; ?>" class="taxonomy-<?php echo $taxonomy; ?> term-<?php echo $term->slug; ?> <?php echo $taxonomy; ?>-<?php echo $term->term_id; ?>" id="<?php echo $term->term_id; ?>" name="<?php echo $taxonomy; ?>"
         <?php echo $selected; ?> />
         <?php echo $term->name; ?></label>
